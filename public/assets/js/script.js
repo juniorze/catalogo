60eb5c839b667d4d49ce609ed9151b008d20e991
juniorze <junior.ze.jw@gmail.com>
2020-01-06 14:39:04 -0300

    CSS - OK

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,5 +1,20 @@
-$(document).ready(function() {
-   if ($('body').height() < $(window).height()) {
-      $('footer').css({ position: 'absolute', bottom: '0px' });
+$(document).on('change', '.up', function() {
+   var names = [];
+   var length = $(this).get(0).files.length;
+   for (var i = 0; i < $(this).get(0).files.length; ++i) {
+      names.push($(this).get(0).files[i].name);
+   }
+   // $("input[name=file]").val(names);
+   if (length > 2) {
+      var fileName = names.join(', ');
+      $(this)
+         .closest('.form-group')
+         .find('.form-control')
+         .attr('value', length + ' files selected');
+   } else {
+      $(this)
+         .closest('.form-group')
+         .find('.form-control')
+         .attr('value', names);
    }
 });
